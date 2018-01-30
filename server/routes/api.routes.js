d8fbf65b0bd37c4b9c5de89e3ad50136805625dd
shiva <shiva@gmail.com>
2018-01-30 13:39:27 +0530

    yes

--- server/routes/api.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-import * as ApiController from '../controllers/api.controller';
-const apiRoute = new Router();
-apiRoute.route('/api/saveportaluser').post(ApiController.Saveportaluser);
-
-export default apiRoute;
