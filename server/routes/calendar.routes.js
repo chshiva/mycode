0379c448c4c13cd6730e1856b99406356782aec9
lucky <lucky@shiva.com>
2018-02-01 13:55:04 +0530

    yes

--- server/routes/calendar.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as CalendarController from '../controllers/calendar.controller';
-
-const calendarRoute = new Router();
-
-
-export default calendarRoute;
