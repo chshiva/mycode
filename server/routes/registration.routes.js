09b305e1ba19b0da8b041bbc7b9637d88a6b854f
lucky <lucky@shiva.com>
2018-02-01 15:58:39 +0530

    dd

--- server/routes/registration.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as RegistrationController from '../controllers/registration.controller';
-
-const registroute = new Router();
-
-registroute.route('/search-corporate/:input').get(RegistrationController.searchCorporate);
-registroute.route('/save-regist-data').post(RegistrationController.saveRegistData);
-
-export default registroute; 
